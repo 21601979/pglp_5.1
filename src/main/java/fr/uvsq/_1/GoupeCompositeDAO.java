5f8fab6890924e1f7f654223e45ef70e4ac2753a
Culerier Tanguy <culerier.tanguy@gmail.com>
2020-04-01 17:16:36 +0200

    implementation des classe PersonnelDAO et GroupeCompositeDAO

--- src/main/java/fr/uvsq/_1/GoupeCompositeDAO.java
+++ src/main/java/fr/uvsq/_1/GoupeCompositeDAO.java
@@ -1,17 +1,39 @@
 package fr.uvsq._1;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class GoupeCompositeDAO extends DAO<GroupeComposite>{
 
     @Override
     public void serialize(GroupeComposite obj, String file) {
-        // TODO Auto-generated method stub
-        
+        ObjectOutputStream out = null;
+        try {
+          final FileOutputStream fichier = new FileOutputStream("test");
+          out = new ObjectOutputStream(fichier);
+          out.writeObject(obj);
+          out.flush();
+        } catch (java.io.IOException e) {
+          e.printStackTrace(); 
+        } 
     }
 
     @Override
     public GroupeComposite deserialize(String file) {
-        // TODO Auto-generated method stub
-        return null;
+        ObjectInputStream in = null;
+        GroupeComposite ret = null;
+        try {
+            final FileInputStream fichier = new FileInputStream("test");
+            in = new ObjectInputStream(fichier);
+            ret = (GroupeComposite) in.readObject();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            }
+        return ret;
     }
 
 }
--- src/main/java/fr/uvsq/_1/PersonnelDAO.java
+++ src/main/java/fr/uvsq/_1/PersonnelDAO.java
@@ -1,16 +1,38 @@
 package fr.uvsq._1;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class PersonnelDAO extends DAO<Personnel>{
 
     @Override
     public void serialize(Personnel obj, String file) {
-        // TODO Auto-generated method stub
-        
+        ObjectOutputStream out = null;
+        try {
+          final FileOutputStream fichier = new FileOutputStream("test");
+          out = new ObjectOutputStream(fichier);
+          out.writeObject(obj);
+          out.flush();
+        } catch (java.io.IOException e) {
+          e.printStackTrace(); 
+        } 
     }
 
     @Override
     public Personnel deserialize(String file) {
-        // TODO Auto-generated method stub
-        return null;
+        ObjectInputStream in = null;
+        Personnel ret = null;
+        try {
+            final FileInputStream fichier = new FileInputStream("test");
+            in = new ObjectInputStream(fichier);
+            ret = (Personnel) in.readObject();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            }
+        return ret;
     }
 }
